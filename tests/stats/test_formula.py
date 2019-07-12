73779b475e384dae507e647fd079c5d4010308c3
Andrii Dobrovolskyi <andrii.dobrovolskyi@sigma.software>
2019-07-12 20:02:41 +0300

    VIQ-1848 Added formulas for historical values calculation

--- tests/stats/test_formula.py
+++ tests/stats/test_formula.py
@@ -1,9 +1,15 @@
 from datetime import datetime
+import math
+
 from unittest import TestCase
 
 from es_components.stats.formula import get_engage_rate
 from es_components.stats.formula import get_linear_value
 from es_components.stats.formula import get_sentiment
+from es_components.stats.formula import get_counter_dataframe
+from es_components.stats.formula import get_counter_dataframe_tailing_sum
+
+from .base import MathTestCase
 
 
 class TestFormulaLinearValue(TestCase):
@@ -115,3 +121,139 @@ class TestFormulaEngageRate(TestCase):
 
         self.assertEqual(expected_engage_rate, engage_rate)
         self.assertTrue(isinstance(engage_rate, float))
+
+
+class TestCumulativeCounter(MathTestCase):
+    def get_diffs(self, history):
+        diffs = [math.nan] + [
+            history[-i - 2] - history[-i - 1]
+            for i in range(len(history) - 1)
+        ]
+
+        return diffs
+
+    def test_dataframe_without_normalization(self):
+        history = [750, 650, 550, 100000, 90000, 81000, 0, 0, 66000, 55000,
+                   51000, 48000, 46000, 45000, 44500, 44250, 44125]
+        expected_diffs = self.get_diffs(history)
+
+        dataframe = get_counter_dataframe(history, constantly_growing=None, max_sigmas=None)
+
+        # has expected fields and length
+        self.assertListEqual(list(dataframe), ["history", "diffs", "is_normal"])
+        self.assertEqual(dataframe.history.count(), len(history))
+
+        # reversed values order
+        self.assertEqual(dataframe.history[0], history[-1])
+        self.assertEqual(dataframe.history[len(history)-1], history[0])
+
+        # all diffs are normal
+        expected_is_normal = [True for _ in expected_diffs]
+        self.assertMathListEqual(expected_is_normal, list(dataframe.is_normal))
+
+        # all diffs are expected
+        self.assertMathListEqual(expected_diffs, list(dataframe.diffs))
+
+    def test_dataframe_normalization_by_constantly_growing(self):
+        history = [750, 650, 550, 100000, 90000, 81000, 0, 0, 66000, 55000,
+                   51000, 48000, 46000, 45000, 44500, 44250, 44125]
+        expected_diffs = self.get_diffs(history)
+
+        dataframe = get_counter_dataframe(history, constantly_growing=True, max_sigmas=None)
+
+        # every diff is normal except negatives
+        expected_is_normal = [math.isnan(diff) or diff >= 0 for diff in expected_diffs]
+        self.assertListEqual(expected_is_normal, list(dataframe.is_normal))
+
+        # all diffs are expected
+        expected_diffs = [math.nan if diff < 0 else diff for diff in expected_diffs]
+        self.assertMathListEqual(expected_diffs, list(dataframe.diffs))
+
+    def test_dataframe_normalization_by_sigmas(self):
+        history = [750, 650, 550, 100000, 90000, 81000, 0, 0, 66000, 55000,
+                   51000, 48000, 46000, 45000, 44500, 44250, 44125]
+        expected_diffs = self.get_diffs(history)
+
+        dataframe = get_counter_dataframe(history, constantly_growing=False, max_sigmas=2)
+
+        # every diff is normal except failed by the 3-sigma rule
+        expected_is_normal = [True for _ in expected_diffs]
+        expected_is_normal[9] = False   # 66000 -> 0
+        expected_is_normal[11] = False  # 0 -> 81000
+        expected_is_normal[14] = False  # 100000 -> 550
+        self.assertMathListEqual(expected_is_normal, list(dataframe.is_normal))
+
+    def test_tailing_sum_1(self):
+        history = [750, 650, 550, 100000, 90000, 81000, 0, 0, 66000, 55000,
+                   51000, 48000, 46000, 45000, 44500, 44250, 44125]
+
+        dataframe = get_counter_dataframe(history, constantly_growing=True, max_sigmas=2)
+
+        expected_value = 100  # (750 - 650)
+        actual_value = get_counter_dataframe_tailing_sum(dataframe, count=1, max_errors=None)
+        self.assertEqual(expected_value, actual_value)
+
+    def test_tailing_sum_2(self):
+        history = [750, 650, 550, 100000, 90000, 81000, 0, 0, 66000, 55000,
+                   51000, 48000, 46000, 45000, 44500, 44250, 44125]
+
+        dataframe = get_counter_dataframe(history, constantly_growing=True, max_sigmas=2)
+
+        # (750 - 650) + (650 - 550)
+        expected_value = 200
+
+        actual_value = get_counter_dataframe_tailing_sum(dataframe, count=2, max_errors=None)
+        self.assertEqual(expected_value, actual_value)
+
+    def test_tailing_sum_10(self):
+        history = [750, 650, 550, 100000, 90000, 81000, 0, 0, 66000, 55000,
+                   51000, 48000, 46000, 45000, 44500, 44250, 44125]
+
+        # = (750 - 650) + (650 - 550) + (#invalid: 550-100k) + (100k - 90k) + (90k - 81k) +
+        # + (#invalid: 81k - 0) + (0 - 0) + (#invalid: 0 - 66k) + (66k - 55k) + (55k - 51k)
+        expected_value = 200 + 10000 + 9000 + 0 + 11000 + 4000
+
+        dataframe = get_counter_dataframe(history, constantly_growing=True, max_sigmas=2)
+
+        actual_value = get_counter_dataframe_tailing_sum(dataframe, count=10, max_errors=None)
+        self.assertEqual(expected_value, actual_value)
+
+    def test_tailing_sum_10_errors_filter(self):
+        history = [750, 650, 550, 100000, 90000, 81000, 0, 0, 66000, 55000,
+                   51000, 48000, 46000, 45000, 44500, 44250, 44125]
+
+        # = (750 - 650) + (650 - 550) + (#invalid: 550-100k) + (100k - 90k) + (90k - 81k) +
+        # + (#invalid: 81k - 0) + (0 - 0) + (#invalid: 0 - 66k) + (66k - 55k) + (55k - 51k)
+        expected_value = 200 + 10000 + 9000 + 0 + 11000 + 4000
+
+        dataframe = get_counter_dataframe(history, constantly_growing=True, max_sigmas=2)
+
+        actual_value = get_counter_dataframe_tailing_sum(dataframe, count=10, max_errors=2)
+        self.assertIsNone(actual_value)
+
+        actual_value = get_counter_dataframe_tailing_sum(dataframe, count=10, max_errors=3)
+        self.assertEqual(expected_value, actual_value)
+
+    def test_tailing_sum_too_wide(self):
+        history = [750, 650, 550, 100000, 90000, 81000, 0, 0, 66000, 55000,
+                   51000, 48000, 46000, 45000, 44500, 44250, 44125]
+
+        # = (750 - 650) + (650 - 550) + (#invalid: 550-100k) + (100k - 90k) + (90k - 81k) +
+        # + (#invalid: 81k - 0) + (0 - 0) + (#invalid: 0 - 66k) + (66k - 55k) + (55k - 51k) +
+        # + (51k - 48k) + (48k - 46k) + (46k - 45k) + (45k - 44.5k) + (44.5k - 44.25k) + (44.25k - 44.125k)
+        expected_value = 200 + 10000 + 9000 + 0 + 11000 + 4000 + 3000 + 2000 + 1000 + 500 + 250 + 125
+
+        dataframe = get_counter_dataframe(history, constantly_growing=True, max_sigmas=2)
+
+        actual_value = get_counter_dataframe_tailing_sum(dataframe, count=len(history) + 10, max_errors=None)
+        self.assertEqual(expected_value, actual_value)
+
+    def test_tailing_sum_cast_type(self):
+        history = [750, 650, 550]
+        expected_type = int
+
+        dataframe = get_counter_dataframe(history)
+        value = get_counter_dataframe_tailing_sum(dataframe, count=1, cast_type=expected_type)
+
+        actual_type = type(value)
+        self.assertEqual(expected_type, actual_type)
--- tests/stats/test_history.py
+++ tests/stats/test_history.py
@@ -1,60 +1,87 @@
-from datetime import datetime
 from unittest import TestCase
 
-from components.channel import ChannelSectionStats
+from es_components.models.channel import ChannelSectionStats
 from es_components.stats import History
 from es_components.stats import HistoryValueError
+
+from es_components.datetime_service import datetime_service
 
 
 class TestHistory(TestCase):
     def test_success(self):
         section = ChannelSectionStats()
         section.subscribers = 100
-        section.views = 1000
 
         # check initial
         self.assertEqual(None, section.fetched_at)
         self.assertEqual(None, section.historydate)
         self.assertEqual([], section.subscribers_history)
-        self.assertEqual([], section.views_history)
 
         # check first update - no history values yet
         history = History(section, ["subscribers", "views"])
-        section.fetched_at = datetime(year=2020, month=1, day=3, hour=12)
+        section.fetched_at = datetime_service.datetime(year=2020, month=1, day=3, hour=12)
         section.subscribers = 301
-        section.views = 3001
         history.update()
 
+        self.assertEqual(
+            datetime_service.datetime(year=2020, month=1, day=2, hour=23, minute=59, second=59),
+            section.historydate
+        )
+        self.assertEqual([], section.subscribers_history)
+
+        # check second update - history contains values
+        history = History(section, ["subscribers", "views"])
+        section.fetched_at = datetime_service.datetime(year=2020, month=1, day=13, hour=12)
+        section.subscribers = 1301
+        history.update()
+
+        expected_historydate = datetime_service.datetime(year=2020, month=1, day=12, hour=23, minute=59, second=59,
+                                                         microsecond=0)
+        self.assertEqual(expected_historydate, section.historydate)
+        self.assertEqual([1250, 1150, 1050, 950, 850, 750, 650, 550, 450, 350], section.subscribers_history)
+
+    def test_same_date(self):
+        section = ChannelSectionStats()
+        section.subscribers = 100
+
+        # check initial
+        self.assertEqual(None, section.fetched_at)
         self.assertEqual(None, section.historydate)
         self.assertEqual([], section.subscribers_history)
-        self.assertEqual([], section.views_history)
 
-        # check second update
-
+        # check first update - no history values yet
         history = History(section, ["subscribers", "views"])
-        section.fetched_at = datetime(year=2020, month=1, day=13, hour=12)
-        section.subscribers = 1301
-        section.views = 13001
+        section.fetched_at = datetime_service.datetime(year=2020, month=1, day=3, hour=12)
+        section.subscribers = 301
         history.update()
 
-        expected_historydate = datetime(year=2020, month=1, day=12, hour=23, minute=59, second=59, microsecond=0)
+        self.assertEqual(
+            datetime_service.datetime(year=2020, month=1, day=2, hour=23, minute=59, second=59),
+            section.historydate
+        )
+        self.assertEqual([], section.subscribers_history)
+
+        # check second update - no history values because of the same date
+        history = History(section, ["subscribers", "views"])
+        section.fetched_at = datetime_service.datetime(year=2020, month=1, day=3, hour=20)
+        section.subscribers = 1301
+        history.update()
+
+        expected_historydate = datetime_service.datetime(year=2020, month=1, day=2, hour=23, minute=59, second=59,
+                                                         microsecond=0)
         self.assertEqual(expected_historydate, section.historydate)
+        self.assertEqual([], section.subscribers_history)
 
-        self.assertEqual([1250, 1150, 1050, 950, 850, 750, 650, 550, 450, 350], section.subscribers_history)
-        self.assertEqual([12500, 11500, 10500, 9500, 8500, 7500, 6500, 5500, 4500, 3500], section.views_history)
-
-
-    def test_exception(self):
+    def test_exception_current_time_less_then_previous(self):
         section = ChannelSectionStats()
-        section.fetched_at = datetime(year=2020, month=1, day=1, hour=12)
+        section.fetched_at = datetime_service.datetime(year=2020, month=1, day=1, hour=12)
 
         history = History(section, ["subscribers"])
-        section.fetched_at = datetime(year=2020, month=1, day=1, hour=0)
+        section.fetched_at = datetime_service.datetime(year=2020, month=1, day=1, hour=0)
         self.assertRaises(HistoryValueError, history.update)
 
     def test_retention_period(self):
         max_retention_perod = 31
-
         section = ChannelSectionStats()
         section.subscribers = 100
         section.views = 1000
@@ -62,16 +89,74 @@ class TestHistory(TestCase):
         history = History(section, ["subscribers"])
         section.subscribers = 301
         section.views = 3001
-        section.fetched_at = datetime(year=2020, month=1, day=3)
+        section.fetched_at = datetime_service.datetime(year=2020, month=1, day=3)
         history.update()
         self.assertEqual(0, len(section.subscribers_history))
 
         history = History(section, ["subscribers"])
-        section.fetched_at = datetime(year=2020, month=1, day=13)
+        section.fetched_at = datetime_service.datetime(year=2020, month=1, day=13)
         history.update()
         self.assertEqual(10, len(section.subscribers_history))
 
         history = History(section, ["subscribers"])
-        section.fetched_at = datetime(year=2021, month=1, day=13)
+        section.fetched_at = datetime_service.datetime(year=2021, month=1, day=13)
         history.update()
         self.assertEqual(max_retention_perod, len(section.subscribers_history))
+
+    def test_missing_values(self):
+        section = ChannelSectionStats()
+        # check 2-nd complete update
+        section.fetched_at = datetime_service.datetime(year=2020, month=1, day=13, hour=12)
+        section.subscribers = 1301
+        section.historydate = datetime_service.datetime(year=2020, month=1, day=12, hour=23, minute=59, second=59,
+                                                        microsecond=0)
+        section.subscribers_history = [1250, 1150, 1050, 950, 850, 750, 650, 550, 450, 350]
+
+        # check 1-st missed value
+        history = History(section, ["subscribers",])
+        section.fetched_at = datetime_service.datetime(year=2020, month=1, day=15, hour=12)
+        section.subscribers = None
+        section.views = 15001
+        history.update()
+
+        expected_historydate = datetime_service.datetime(year=2020, month=1, day=14, hour=23, minute=59, second=59,
+                                                         microsecond=0)
+        self.assertEqual(expected_historydate, section.historydate)
+        self.assertEqual([None, None, 1250, 1150, 1050, 950, 850, 750, 650, 550, 450, 350],
+                         section.subscribers_history)
+
+        # check 2-nd missed value
+        history = History(section, ["subscribers"])
+        section.fetched_at = datetime_service.datetime(year=2020, month=1, day=16, hour=12)
+        section.subscribers = None
+        history.update()
+
+        expected_historydate = datetime_service.datetime(year=2020, month=1, day=15, hour=23, minute=59, second=59,
+                                                         microsecond=0)
+        self.assertEqual(expected_historydate, section.historydate)
+        self.assertEqual([None, None, None, 1250, 1150, 1050, 950, 850, 750, 650, 550, 450, 350],
+                         section.subscribers_history)
+
+        # check 3-nd complete update
+        history = History(section, ["subscribers"])
+        section.fetched_at = datetime_service.datetime(year=2020, month=1, day=18, hour=12)
+        section.subscribers = 1801
+        history.update()
+
+        expected_historydate = datetime_service.datetime(year=2020, month=1, day=17, hour=23, minute=59, second=59,
+                                                         microsecond=0)
+        self.assertEqual(expected_historydate, section.historydate)
+        self.assertEqual([1750, 1650, 1550, 1450, 1350, 1250, 1150, 1050, 950, 850, 750, 650, 550, 450, 350],
+                         section.subscribers_history)
+
+        # check missed all values
+        history = History(section, ["subscribers"])
+        section.fetched_at = datetime_service.datetime(year=2020, month=2, day=18, hour=12)
+        section.subscribers = None
+        section.views = None
+        history.update()
+
+        expected_historydate = datetime_service.datetime(year=2020, month=2, day=17, hour=23, minute=59, second=59,
+                                                         microsecond=0)
+        self.assertEqual(expected_historydate, section.historydate)
+        self.assertEqual([], section.subscribers_history)
