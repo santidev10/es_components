c3120690d82131c0cf84721f3d10831b0b97ac82
Kharko, Yaryna <yaryna.kharko@sigma.software>
2019-07-15 13:53:04 +0300

    VIQ-1989 Added methods to managers

--- managers/keyword.py
+++ managers/keyword.py
@@ -7,6 +7,7 @@ class KeywordManager(BaseManager):
     allowed_sections = BaseManager.allowed_sections + (Sections.STATS, Sections.STATS_SCHEDULE)
     model = Keyword
 
-    def forced_filters(self, updated_at):
+    def forced_filters(self, updated_at=None):
         return super(KeywordManager, self).forced_filters(updated_at) &\
-               self._filter_existent_section(Sections.STATS)
+               self.filter_existent_section(Sections.STATS)
+
