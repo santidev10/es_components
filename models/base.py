f462b3f5c003be38895e30768c716e58c574c686
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2019-07-23 22:23:17 +0300

    VIQ-2008: Add segments section

--- models/base.py
+++ models/base.py
@@ -8,6 +8,7 @@ from elasticsearch_dsl import Object
 from elasticsearch_dsl import Text
 from elasticsearch_dsl.utils import AttrList
 
+from es_components.constants import Sections
 from es_components.stats import History
 
 
@@ -87,9 +88,14 @@ class Deleted(BaseInnerDoc):
     reason = Text(index=False)
 
 
+class CommonSegmentSection(BaseInnerDoc):
+    uuid = Keyword(multi=True)
+
+
 class BaseDocument(Document):
     main = Object(MainSection)
     deleted = Object(Deleted)
+    segments = Object(CommonSegmentSection)
 
     # pylint: disable=redefined-builtin
     def __init__(self, id=None, **kwargs):
@@ -138,3 +144,5 @@ class BaseDocument(Document):
                 values = AttrList(set(list(values) + extra_values))
                 setattr(section, name, values)
 
+    def populate_segments(self, **kwargs):
+        self._populate_section(Sections.SEGMENTS, **kwargs)
