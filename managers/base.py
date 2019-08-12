d95f34fcc9e5072a2dccc7fc5be3a6788ff0d966
Andrii Dobrovolskyi <andrii.dobrovolskyi@sigma.software>
2019-08-12 11:56:27 +0300

    VIQ-2235 code tuning

--- managers/base.py
+++ managers/base.py
@@ -253,7 +253,10 @@ class BaseManager:
         return self._filter_nonexistent_section(Sections.DELETED)
 
     def forced_filters(self):
-        updated_at = f"now-{self.forced_filter_oudated_days*86400}s/s"
+        # "now-1d/d" time format is used
+        # it avoids being tied to the current point in time and makes it possible to cache request/response
+        outdated_seconds = self.forced_filter_oudated_days * 86400
+        updated_at = f"now-{outdated_seconds}s/s"
         field_updated_at = f"{Sections.MAIN}.{TimestampFields.UPDATED_AT}"
         filter_range = QueryBuilder().build().must().range().field(field_updated_at) \
             .gt(updated_at).get()
