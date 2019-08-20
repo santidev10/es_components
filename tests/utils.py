6d14e4009179a548ee31861bc3645eecfdc92846
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2019-08-20 15:40:52 +0300

    Update ES test to not run upon RC environment

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,5 @@
 import os
+import re
 import uuid
 from unittest import TestCase
 from unittest.mock import PropertyMock
@@ -15,12 +16,22 @@ from es_components.models.base import BaseDocument
 class ESTestCase(TestCase):
     _patches = []
 
+    def __init__(self, *args, **kwargs):
+        super(ESTestCase, self).__init__(*args, **kwargs)
+        init_es_connection()
+        self.__dangerous_environment_check()
+
+    def __dangerous_environment_check(self):
+        hosts = [item["host"] for item in connections.get_connection("default").transport.hosts]
+        dangerous_hosts = (
+            r'prod',
+            r'rc',
+        )
+        if any([any([re.search(host_mask, host) for host_mask in dangerous_hosts]) for host in hosts]):
+            raise ConnectionError("Testing on dangerous ElasticSearch host detected")
+
     @classmethod
     def setUpClass(cls):
-        init_es_connection()
-        hosts = [item["host"] for item in connections.get_connection("default").transport.hosts]
-        if any(["prod" in host for host in hosts]):
-            raise ConnectionError("Testing on prod env detected")
         for model_cls in BaseDocument.__subclasses__():
             # pylint: disable=protected-access
             test_index_name = f"test_{model_cls.Index.name}_{uuid.uuid4()}"
