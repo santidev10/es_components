7b4f6337660b61ca145bc4c02e0c6574279690a8
Kharko, Yaryna <yaryna.kharko@sigma.software>
2019-10-29 11:51:49 +0200

    VIQ-2616 Update monitor adding alerts

--- managers/channel.py
+++ managers/channel.py
@@ -3,6 +3,7 @@ from es_components.models.channel import Channel
 from es_components.constants import Sections
 from es_components.constants import CONTENT_OWNER_ID_FIELD
 from es_components.query_builder import QueryBuilder
+from es_components.monitor import Emergency
 from es_components.monitor import Warnings
 from es_components.utils import add_brand_safety_labels
 
@@ -161,4 +162,8 @@ class ChannelManager(BaseManager):
         )
         return warning_few_records_updated + warning_no_new_sections + \
                super(ChannelManager, self)._get_enabled_monitoring_warnings()
+
+    def _get_enabled_monitoring_alerts(self):
+        return (Emergency.NoneRecordsUpdated((Sections.GENERAL_DATA, Sections.STATS), 1),) + \
+               super(ChannelManager, self)._get_enabled_monitoring_alerts()
 
