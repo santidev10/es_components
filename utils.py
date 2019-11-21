592a07300307cb65bc01affc9805ded559269963
George Su <george.su@channelfactory.com>
2019-11-21 15:30:59 -0800

    Added newline for style.

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from itertools import count
 from itertools import groupby
 
+
 def chunks(iterable, size):
     chunk = count()
     for _, group in groupby(iterable, lambda _: next(chunk) // size):
