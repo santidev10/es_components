d7335e58dcf6aa120ce71cd4974241d42c2b660c
George Su <george.su@channelfactory.com>
2020-05-18 11:26:31 -0700

    Renamed custom_properties.is_tracked Aggregation to 'Tracked Channels'.

--- managers/channel.py
+++ managers/channel.py
@@ -174,7 +174,7 @@ class ChannelManager(BaseManager):
 
     def adapt_is_tracked_aggregation(self, aggregations):
         if "custom_properties.is_tracked" in aggregations:
-            aggregations["custom_properties.is_tracked"]["buckets"][0]["key"] = "Vetted Channels"
+            aggregations["custom_properties.is_tracked"]["buckets"][0]["key"] = "Tracked Channels"
         return aggregations
 
     def adapt_channel_group(self, aggregations):
