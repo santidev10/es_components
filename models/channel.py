de0b0d843c62ed504acbfef2b7ea5ee45e98c873
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2020-07-03 23:26:37 +0300

    Remove index refresh interval from code

--- models/channel.py
+++ models/channel.py
@@ -10,7 +10,6 @@ from elasticsearch_dsl import Text
 from es_components.config import CHANNEL_DOC_TYPE
 from es_components.config import CHANNEL_INDEX_NAME
 from es_components.config import CHANNEL_INDEX_PREFIX
-from es_components.config import CHANNEL_REFRESH_INTERVAL
 from es_components.constants import Sections
 from es_components.models.base import BaseDocument
 from es_components.models.base import BaseInnerDoc
@@ -225,9 +224,7 @@ class Channel(BaseDocument):
     class Index:
         name = CHANNEL_INDEX_NAME
         prefix = CHANNEL_INDEX_PREFIX
-        settings = dict(
-            refresh_interval=CHANNEL_REFRESH_INTERVAL,
-        )
+        settings = dict()
 
     class Meta:
         doc_type = CHANNEL_DOC_TYPE
