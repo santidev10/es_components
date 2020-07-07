c78545ad75f3d2c012c6d948e0c42fefec291418
George Su <george.su@channelfactory.com>
2020-07-07 10:53:05 -0700

    Fixed bug where 'Tracked Channels' aggregation wasn't adapted properly.

--- managers/channel.py
+++ managers/channel.py
@@ -168,6 +168,7 @@ class ChannelManager(BaseManager):
     def adapt_is_tracked_aggregation(self, aggregations):
         if "custom_properties.is_tracked" in aggregations:
             aggregations["custom_properties.is_tracked"]["buckets"][0]["key"] = "Tracked Channels"
+            aggregations["custom_properties.is_tracked"]["buckets"].pop(1)
         return aggregations
 
     def _get_enabled_monitoring_warnings(self):
