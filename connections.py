a8d1cb51e5ca570f275442954d02b690fe5a9681
Abe Grair <abe.grair@channelfactory.com>
2021-03-11 11:30:35 -0800

    Progress VIQ2-978:
    - Add python library "requests-aws4auth" to requirements.txt file
    - Update the ES configurations to use AWS IAM access and secret keys of a user that has ES permissions
    - There will be other changes for "restapi" and "dmp" that should be merged at the same time with this branch
    - We need to update these config files for RC to set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY environment variables:
    1- S3 > chf-config-files > restapi > chf.restapi.env.rc
    2- S3 > chf-config-files > dmp > dmp.config.rc

--- connections.py
+++ connections.py
@@ -2,9 +2,14 @@ import certifi
 
 from elasticsearch_dsl import connections
 
+from elasticsearch import RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+
 from es_components.config import ELASTIC_SEARCH_URLS
 from es_components.config import ELASTIC_SEARCH_TIMEOUT
 from es_components.config import ELASTIC_SEARCH_USE_SSL
+from es_components.config import AWS_ES_ACCESS_KEY_ID
+from es_components.config import AWS_ES_SECRET_ACCESS_KEY
 
 
 def init_es_connection():
@@ -13,7 +18,10 @@ def init_es_connection():
             "hosts": ELASTIC_SEARCH_URLS,
             "timeout": ELASTIC_SEARCH_TIMEOUT,
             "use_ssl": ELASTIC_SEARCH_USE_SSL,
-            "ca_certs": certifi.where()
+            "ca_certs": certifi.where(),
+            "http_auth": AWS4Auth(AWS_ES_ACCESS_KEY_ID, AWS_ES_SECRET_ACCESS_KEY, 'us-east-1', 'es'),
+            "use_ssl": True,
+            "verify_certs": True,
+            "connection_class": RequestsHttpConnection
         }
-
     )
