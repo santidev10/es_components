9d5dcf5552e5883276de68aab98c8a37653aa11e
Abe Grair <abe.grair@channelfactory.com>
2021-04-27 10:52:16 -0700

    - Update the ES configurations to use AWS IAM access and secret keys of a user that has ES permissions - There will be other changes for "restapi" and "dmp" that should be merged at the same time with this branch - We need to update these config files for RC to set AWS_ES_ACCESS_KEY_ID, AWS_ES_SECRET_ACCESS_KEY environment variables: 1- S3 > chf-config-files > restapi > chf.restapi.env.rc 2- S3 > chf-config-files > dmp > dmp.config.rc

--- connections.py
+++ connections.py
@@ -9,13 +9,20 @@ from es_components.config import ELASTIC_SEARCH_URLS
 from es_components.config import ELASTIC_SEARCH_TIMEOUT
 from es_components.config import ELASTIC_SEARCH_USE_SSL
 
+from es_components.config import AWS_ES_ACCESS_KEY_ID
+from es_components.config import AWS_ES_SECRET_ACCESS_KEY
+
 
 def init_es_connection():
-    connections.configure(
-        default={
-            "hosts": ELASTIC_SEARCH_URLS,
-            "timeout": ELASTIC_SEARCH_TIMEOUT,
-            "use_ssl": ELASTIC_SEARCH_USE_SSL,
-            "ca_certs": certifi.where()
-        }
-    )
+    es_connection_config = {
+        "hosts": ELASTIC_SEARCH_URLS,
+        "timeout": ELASTIC_SEARCH_TIMEOUT,
+        "use_ssl": ELASTIC_SEARCH_USE_SSL,
+        "ca_certs": certifi.where()
+    }
+    if AWS_ES_ACCESS_KEY_ID and AWS_ES_SECRET_ACCESS_KEY:
+        es_connection_config["http_auth"] = AWS4Auth(AWS_ES_ACCESS_KEY_ID, AWS_ES_SECRET_ACCESS_KEY, 'us-east-1', 'es')
+        es_connection_config["verify_certs"] = True
+        es_connection_config["connection_class"] = RequestsHttpConnection
+
+    connections.configure(default=es_connection_config)
