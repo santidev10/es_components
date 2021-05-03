b7658130aae0f4e11a8146063de9a4d671651c15
Andrew Wong <andrew.wong@channelfactory.com>
2021-05-03 16:31:41 -0700

    Add manager get by video id query for transcriptmanager.

--- managers/transcript.py
+++ managers/transcript.py
@@ -1,6 +1,7 @@
 from es_components.constants import Sections
 from es_components.managers.base import BaseManager
 from es_components.models.transcript import Transcript
+from es_components.query_builder import QueryBuilder
 
 
 class TranscriptManager(BaseManager):
@@ -9,3 +10,7 @@ class TranscriptManager(BaseManager):
                        + (Sections.GENERAL_DATA, Sections.VIDEO, Sections.TEXT)
 
     model = Transcript
+
+    def get_by_video_ids(self, video_ids: list):
+        query = QueryBuilder().build().must().terms().field(f"{Sections.VIDEO}.id").value(video_ids).get()
+        return self.search(query=query)
